2d6a9b3113facb6061343822b6957aeea6698893
yudai-simoma <cdcdasdll@icloud.com>
2022-12-31 22:53:45 +0900

    component追加
    profile,skill,portfolio,background information,contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';//ngModel
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SkillComponent } from './skill/skill.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { BackgroundInformationComponent } from './background-information/background-information.component';
+import { ContactComponent } from './contact/contact.component';
+import { DynamicTableComponent } from './dynamic-table/dynamic-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    SkillComponent,
+    PortfolioComponent,
+    BackgroundInformationComponent,
+    ContactComponent,
+    DynamicTableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule//ngModel
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [DynamicTableComponent]
 })
 export class AppModule { }
