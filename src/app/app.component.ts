f07e832dbe4668b7f95984e228ff7b6995a6c5f1
yudai-simoma <cdcdasdll@icloud.com>
2023-01-03 04:43:17 +0900

    追加_動的コンポーネント処理作成

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
-// import { Component, ComponentFactory, ComponentFactoryResolver, OnInit, ViewContainerRef } from '@angular/core';
-// import { DynamicTableComponent } from './dynamic-table/dynamic-table.component';
+import { Component, ComponentFactory, ComponentFactoryResolver, OnInit, ViewContainerRef, ViewChild } from '@angular/core';
+import { DynamicTableComponent } from './dynamic-table/dynamic-table.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title: String = "yudai shimoma portfolio";
   options: String = "1,profile 2,skill 3,portfolio 4,background information 5,contact";
   err_meg: String = "番はありません。";
@@ -19,19 +18,28 @@ export class AppComponent {
   enter_flg_4: boolean = false;
   enter_flg_5: boolean = false;
   enter_flg_err: boolean = false;
-  // factory: ComponentFactory<DynamicTableComponent>;
+  factory!: ComponentFactory<DynamicTableComponent>;
+  loop_count: number = -1;
+  input_flg = false;
 
-  // constructor(
-  //   public viewContainerRef: ViewContainerRef,
-  //   private resolver: ComponentFactoryResolver) { }
+  // @ViewChild を追加
+  @ViewChild('dynamic', { read: ViewContainerRef }) viewContainerRef!: ViewContainerRef;
+
+  constructor(
+    private resolver: ComponentFactoryResolver) { }
 
   ngOnInit(): void {
     document.getElementById('first_focus')!.focus();
-    // this.factory = this.resolver.resolveComponentFactory(DynamicTableComponent);
+    this.factory = this.resolver.resolveComponentFactory(DynamicTableComponent);
   }
 
   onEnter() {
-    this.clear_flg();
+    this.input_flg = true;
+    if (this.select_number == "42") {
+      document.location.href = "https://42tokyo.jp/";
+      return;
+    }
+    this.add_dynamic();
     if (this.select_number == "1") {
       this.enter_flg_1 = true;
       return;
@@ -52,10 +60,6 @@ export class AppComponent {
       this.enter_flg_5 = true;
       return;
     }
-    if (this.select_number == "42") {
-      document.location.href = "https://42tokyo.jp/";
-      return;
-    }
     if (this.select_number == "") {
       this.enter_flg_err = true;
       this.err_meg = "";
@@ -63,16 +67,10 @@ export class AppComponent {
       this.enter_flg_err = true;
       this.err_meg = this.select_number + "番はありません。";
     }
-    // this.viewContainerRef.createComponent(this.factory);
   }
 
-  clear_flg() {
-    this.enter_flg_1 = false;
-    this.enter_flg_2 = false;
-    this.enter_flg_3 = false;
-    this.enter_flg_4 = false;
-    this.enter_flg_5 = false;
-    this.enter_flg_err = false;
-    this.err_meg = "番はありません。";
+  add_dynamic() {
+    this.loop_count++;
+    this.viewContainerRef.createComponent(this.factory);
   }
 } 
---
+++ src/app/dynamic-table/dynamic-table.component.ts
@@ -0,0 +1,71 @@
+import { Component, AfterViewInit } from '@angular/core';
+import { AppComponent } from '../app.component';
+
+@Component({
+  selector: 'app-dynamic-table',
+  templateUrl: './dynamic-table.component.html',
+  styleUrls: ['./dynamic-table.component.css']
+})
+export class DynamicTableComponent implements AfterViewInit {
+
+  err_meg: String = "番はありません。";
+  question_meg: String = "何番を見ますか？番号を入力してください。";
+  select_number: String = "";
+  enter_flg_1: boolean = false;
+  enter_flg_2: boolean = false;
+  enter_flg_3: boolean = false;
+  enter_flg_4: boolean = false;
+  enter_flg_5: boolean = false;
+  enter_flg_err: boolean = false;
+  loop_count: number = 0;
+  input_flg = false;
+
+  constructor(
+    private appComponent: AppComponent) {
+  }
+
+  async ngOnInit() {
+    this.loop_count = await this.appComponent.loop_count;
+  }
+
+  ngAfterViewInit() {
+    document.getElementById("loop_focus_" + this.loop_count)!.focus();
+  }
+
+  onEnter() {
+    this.input_flg = true;
+    this.appComponent.add_dynamic();
+    if (this.select_number == "1") {
+      this.enter_flg_1 = true;
+      return;
+    }
+    if (this.select_number == "2") {
+      this.enter_flg_2 = true;
+      return;
+    }
+    if (this.select_number == "3") {
+      this.enter_flg_3 = true;
+      return;
+    }
+    if (this.select_number == "4") {
+      this.enter_flg_4 = true;
+      return;
+    }
+    if (this.select_number == "5") {
+      this.enter_flg_5 = true;
+      return;
+    }
+    if (this.select_number == "42") {
+      document.location.href = "https://42tokyo.jp/";
+      return;
+    }
+    if (this.select_number == "") {
+      this.enter_flg_err = true;
+      this.err_meg = "";
+    } else {
+      this.enter_flg_err = true;
+      this.err_meg = this.select_number + "番はありません。";
+    }
+  }
+
+}
