8ecff47250b544b82bf839fb06870481de09b798
yudai-simoma <cdcdasdll@icloud.com>
2023-01-03 05:57:43 +0900

    リファクタリング

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,42 +6,58 @@ import { DynamicTableComponent } from './dynamic-table/dynamic-table.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit {
-  title: String = "yudai shimoma portfolio";
-  options: String = "1,profile 2,skill 3,portfolio 4,background information 5,contact";
+  //画面メッセージ変数
+  title: String = "YUDAI SHIMOMA PORTFOLIO SITE";
+  // title: String = "yudai shimoma portfolio site";
+  options: String = "1,自己紹介 2,スキル 3,作品集 4,プライベート情報 5,お問い合わせ";
   err_meg: String = "番はありません。";
   question_meg: String = "何番を見ますか？番号を入力してください。";
-  select_number: String = "";
+  select_number: String = ""; //番号入力変数
+  //表示箇所のフラグ変数
   enter_flg_1: boolean = false;
   enter_flg_2: boolean = false;
   enter_flg_3: boolean = false;
   enter_flg_4: boolean = false;
   enter_flg_5: boolean = false;
   enter_flg_err: boolean = false;
+  input_flg = false;  //活性・非活性変数
+  //動的コンポーネントに使用する変数
   factory!: ComponentFactory<DynamicTableComponent>;
   loop_count: number = -1;
-  input_flg = false;
 
-  // @ViewChild を追加
+  // @ViewChild を追加、動的コンポーネントに使用
   @ViewChild('dynamic', { read: ViewContainerRef }) viewContainerRef!: ViewContainerRef;
 
   constructor(
     private resolver: ComponentFactoryResolver) { }
 
+  /**
+   * 初期表示処理
+   */
   ngOnInit(): void {
     document.getElementById('first_focus')!.focus();
+    //動的コンポーネントに使用
     this.factory = this.resolver.resolveComponentFactory(DynamicTableComponent);
   }
 
+  /**
+   * エンターキーが押された際の処理
+   * @param {any} event エンターキー押下時の情報を格納
+   */
   onEnter(event: any) {
+    //エンターが日本語変換時のものか判断
     if (event.keyCode != 13) {
       return;
     }
+    //一度入力した欄は、非活性にする
     this.input_flg = true;
     if (this.select_number == "42") {
       document.location.href = "https://42tokyo.jp/";
       return;
     }
+    //動的コンポーネント作成
     this.add_dynamic();
     if (this.select_number == "1") {
       this.enter_flg_1 = true;
@@ -72,6 +88,9 @@ export class AppComponent implements OnInit {
     }
   }
 
+  /**
+   * 動的コンポーネンんと作成処理
+   */
   add_dynamic() {
     this.loop_count++;
     this.viewContainerRef.createComponent(this.factory);
--- src/app/dynamic-table/dynamic-table.component.ts
+++ src/app/dynamic-table/dynamic-table.component.ts
@@ -6,42 +6,58 @@ import { AppComponent } from '../app.component';
   templateUrl: './dynamic-table.component.html',
   styleUrls: ['./dynamic-table.component.css']
 })
-export class DynamicTableComponent implements AfterViewInit {
 
+export class DynamicTableComponent implements AfterViewInit {
+  //画面メッセージ変数
   err_meg: String = "番はありません。";
   question_meg: String = "何番を見ますか？番号を入力してください。";
-  select_number: String = "";
+  select_number: String = ""; //番号入力変数
+  //表示箇所のフラグ変数
   enter_flg_1: boolean = false;
   enter_flg_2: boolean = false;
   enter_flg_3: boolean = false;
   enter_flg_4: boolean = false;
   enter_flg_5: boolean = false;
   enter_flg_err: boolean = false;
+  input_flg = false;  //活性・非活性変数
+  //動的コンポーネントに使用する変数
   loop_count: number = -1; //0だとloop_count==0の時しかフォーカスが当たらないため-1
-  input_flg = false;
 
   constructor(
     private appComponent: AppComponent) {
   }
 
+  /**
+   * 初期表示処理
+   */
   async ngOnInit() {
     this.loop_count = await this.appComponent.loop_count;
     window.scrollBy(0, 100);
   }
 
+  /**
+   * 初期表示処理後すぐの処理
+   */
   ngAfterViewInit() {
     document.getElementById("loop_focus_" + this.loop_count)!.focus();
   }
 
+  /**
+   * エンターキーが押された際の処理
+   * @param {any} event エンターキー押下時の情報を格納
+   */
   onEnter(event: any) {
+    //エンターが日本語変換時のものか判断
     if (event.keyCode != 13) {
       return;
     }
+    //一度入力した欄は、非活性にする
     this.input_flg = true;
     if (this.select_number == "42") {
       document.location.href = "https://42tokyo.jp/";
       return;
     }
+    //動的コンポーネント作成
     this.appComponent.add_dynamic();
     if (this.select_number == "1") {
       this.enter_flg_1 = true;
@@ -71,5 +87,4 @@ export class DynamicTableComponent implements AfterViewInit {
       this.err_meg = this.select_number + "番はありません。";
     }
   }
-
 }
